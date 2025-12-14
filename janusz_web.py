7aa85b675023f7d96a458c46c582a511096de019
maniekwro-pixel <maniek.wro@gmail.com>
2025-12-15 00:12:42 +0100

    Update janusz_web.py

--- janusz_web.py
+++ janusz_web.py
@@ -33,7 +33,7 @@ except Exception as e:
 def get_janusz_response(user_input, attachment=None):
     try:
         model = genai.GenerativeModel(
-            model_name="gemini-1.5-flash", 
+            model_name="gemini-1.5-flash-latest", 
             system_instruction="""
             Jesteś Januszem, księgowym.
             Analizuj dokumenty: Data, Sprzedawca, Kwota Brutto.
@@ -148,5 +148,6 @@ if run_btn:
                 if save_to_google_sheets(user_prompt, resp, kwota):
                     st.toast("✅ Zapisano w Arkuszu Google!", icon="📂")
                     st.balloons()
+
 
 
